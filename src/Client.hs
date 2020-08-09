31c32c066a229139c4e860949becb83ad7fd89ba
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-08-09 03:10:51 -0600

    WIP run in ghci

--- src/Client.hs
+++ src/Client.hs
@@ -1,32 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications  #-}
-
-
 module Main where
 
 
-import           Data.Text
-import           Language.Javascript.JSaddle (MonadJSM, liftJSM, eval)
-import           Shpadoinkle
-import           Shpadoinkle.Backend.ParDiff
-import           Shpadoinkle.Html
-import           Shpadoinkle.Html.LocalStorage
-import           Shpadoinkle.Router
-
-import           Types
-import           View
+import qualified ClientMain as ClientMain
 
 
-initialState :: MonadJSM m => ZettelEditor m => Route -> m ViewModel
-initialState r = do
-  msid <- getStorage (LocalStorageKey "session")
-  case msid of
-    Just sid -> initialModel r <$> getDatabase sid
-    Nothing -> do
-      liftJSM $ eval ("window.localStorage.removeItem('session')" :: Text)
-      return $ initialModel LoginRoute emptyZettel
-
-
-main :: IO ()
-main = runJSorWarp 8080 $
-  fullPageSPA @SPA runApp runParDiff initialState view getBody (return . router) routes
+main = ClientMain.main
